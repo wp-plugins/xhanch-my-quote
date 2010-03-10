f6b2a132da8a676c55da1d30f82a7869167f6832
xhanch_studio <xhanch_studio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-10 13:27:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/xhanch-my-quote/trunk@215709 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ db/john-lennon.db.php
@@ -0,0 +1,19 @@
+<?php
+	$db = array(
+		'Love is a promise, love is a souvenir, once given never forgotten, never let it disappear. <br/><i>- John Lennon -</i>',
+		'I remember a time when everybody I loved hated me because I hated them. - Letter to Stuart Sutcliffe circa 1960. <br/><i>- John Lennon -</i>',
+		'Lots of people who complained about us receiving the MBE received theirs for heroism in the war --for killing people. We received ours for entertaining other people. I\'d say we deserve ours more. <br/><i>- John Lennon -</i>',
+		'It\'s fear of the unknown. The unknown is what it is. And to be frightened of it is what sends everybody scurrying around chasing dreams, illusions, wars, peace, love, hate, all that--it\'s all illusion. Unknown is what it is. Accept that it\'s unknown and it\'s plain sailing. Everything is unknown--then you\'re ahead of the game. That\'s what it is. Right? <br/><i>- John Lennon -</i>',
+		'The pressures of being a parent are equal to any pressure on earth. To be a conscious parent, and really look to that little being\'s mental and physical health, is a responsibility which most of us, including me, avoid most of the time because it\'s too hard. <br/><i>- John Lennon -</i>',
+		'Work is life, you know, and without it, there\'s nothing but fear and insecurity. <br/><i>- John Lennon -</i>',
+		'I\'ve had the boyhood thing of being Elvis. Now I want to be with my best friend, and my best friend\'s my wife. Who could ask for anything more? <br/><i>- John Lennon -</i>',
+		'When I hold you in my arms and I feel my finger on your trigger I know no one can do me no harm because happiness is a warm gun. <br/><i>- John Lennon -</i>',
+		'My defenses were so great. The cocky rock and roll hero who knows all the answers was actually a terrified guy who didn\'t know how to cry. Simple. <br/><i>- John Lennon -</i>',
+		'The worst drugs are as bad as anybody\'s told you. It\'s just a dumb trip, which I can\'t condemn people if they get into it, because one gets into it for one\'s own personal, social, emotional reasons. It\'s something to be avoided if one can help it. <br/><i>- John Lennon -</i>',
+		'The older generation are leading this country to galloping ruin! <br/><i>- John Lennon -</i>',
+		'Christianity will go. It will vanish and shrink. I needn\'t argue with that; I\'m right and I will be proved right. We\'re more popular than Jesus now; I don\'t know which will go first - rock and roll or Christianity. <br/><i>- John Lennon -</i>',
+		'All you need is love. <br/><i>- John Lennon -</i>',
+		'And so this is Xmas for black and for white, for yellow and red, let\'s stop all the fight. <br/><i>- John Lennon -</i>',
+		'The pressures of being a parent are equal to any pressure on earth. To be a conscious parent, and really look to that little being\'s mental and physical health, is a responsibility which most of us, including me, avoid most of the time because it\'s too hard. <br/><i>- John Lennon -</i>',
+	)
+?>
