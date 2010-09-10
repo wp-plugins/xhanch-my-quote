89a3eb27b278113ca0cb705579e5d86d0d76a8df
xhanch_studio <xhanch_studio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-10 11:06:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/xhanch-my-quote/trunk@287741 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ db/sun-tzu.db.php
@@ -0,0 +1,24 @@
+<?php
+	$db = array(
+		'All men can see these tactics whereby I conquer, but what none can see is the strategy out of which victory is evolved. <br/><i>- Sun Tzu -</i>',
+		'All war is based on deception. <br/><i>- Sun Tzu -</i>',
+		'All war is deception. <br/><i>- Sun Tzu -</i>',
+		'All warfare is based on deception. <br/><i>- Sun Tzu -</i>',
+		'Be extremely subtle, even to the point of formlessness. Be extremely mysterious, even to the point of soundlessness. Thereby you can be the director of the opponent\'s fate. <br/><i>- Sun Tzu -</i>',
+		'Can you imagine what I would do if I could do all I can? <br/><i>- Sun Tzu -</i>',
+		'Confront them with annihilation, and they will then survive; plunge them into a deadly situation, and they will then live. When people fall into danger, they are then able to strive for victory. <br/><i>- Sun Tzu -</i>',
+		'For them to perceive the advantage of defeating the enemy, they must also have their rewards. <br/><i>- Sun Tzu -</i>',
+		'For to win one hundred victories in one hundred battles is not the acme of skill. To subdue the enemy without fighting is the acme of skill. <br/><i>- Sun Tzu -</i>',
+		'He who is prudent and lies in wait for an enemy who is not, will be victorious. <br/><i>- Sun Tzu -</i>',
+		'He who knows when he can fight and when he cannot, will be victorious. <br/><i>- Sun Tzu -</i>',
+		'Hence that general is skilful in attack whose opponent does not know what to defend; and he is skilful in defense whose opponent does not know what to attack. <br/><i>- Sun Tzu -</i>',
+		'If ignorant both of your enemy and yourself, you are certain to be in peril. <br/><i>- Sun Tzu -</i>',
+		'If our soldiers are not overburdened with money, it is not because they have a distaste for riches; if their lives are not unduly long, it is not because they are disinclined to longevity. <br/><i>- Sun Tzu -</i>',
+		'If you are far from the enemy, make him believe you are near. <br/><i>- Sun Tzu -</i>',
+		'If you know the enemy and know yourself you need not fear the results of a hundred battles. <br/><i>- Sun Tzu -</i>',
+		'In the practical art of war, the best thing of all is to take the enemy\'s country whole and intact; to shatter and destroy it is not so good. <br/><i>- Sun Tzu -</i>',
+		'Invincibility lies in the defence; the possibility of victory in the attack. <br/><i>- Sun Tzu -</i>',
+		'It is essential to seek out enemy agents who have come to conduct espionage against you and to bribe them to serve you. Give them instructions and care for them. Thus doubled agents are recruited and used. <br/><i>- Sun Tzu -</i>',
+		'It is only the enlightened ruler and the wise general who will use the highest intelligence of the army for the purposes of spying, and thereby they achieve great results. <br/><i>- Sun Tzu -</i>',
+	);
+?>
