22800e115ef7fc893a77820d1e924853c207ded3
xhanch_studio <xhanch_studio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-30 11:57:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/xhanch-my-quote/trunk@295155 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- xhanch_my_quote.php
+++ xhanch_my_quote.php
@@ -5,7 +5,7 @@
 		Description: Random Quote plugin for wordpress
 		Author: Susanto BSc (Xhanch Studio)
 		Author URI: http://xhanch.com
-		Version: 1.4.4
+		Version: 1.4.5
 	*/
 
 	function xhanch_my_quote_install () {
